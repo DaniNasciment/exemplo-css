6d4a1b2c26d719f9a3dd01d977149444574e41b6
DaniNasciment <dannni01@hotmail.com>
2022-11-18 10:49:29 -0300

    feat:add dom

---
+++ script.js
@@ -0,0 +1,11 @@
+function handleClick() {
+    document.querySelector('.titulo').innerHTML = 'Argentina errou';
+} 
+
+function handleMove() {
+    document.querySelector('.titulo').innerHTML = 'Argentina errou';
+}  
+
+function handleOut() {
+    document.querySelector('.titulo').innerHTML = 'Campanha Germán Cano na seleção Argentina';
+} 
